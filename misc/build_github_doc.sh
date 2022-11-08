3e1da20a9daa4000f546ac4e08dac82fa1589f4b
Anders Henja <anders@baltrad.eu>
2022-11-08 12:40:32 +0100

    Added some dependencies for docs

--- misc/build_github_doc.sh
+++ misc/build_github_doc.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+set -x
+
 PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
+
+# First setup environment
+apt-get update
+apt-get -y install git rsync python3-sphinx
+
+git config --local user.email "baltrad@users.noreply.github.com"
+git config --local user.name "Baltrad GitHub Action"
 
 cd "$PROJECT_ROOT/doc"
 
