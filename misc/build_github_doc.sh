01ce63d759e2afca663c030add1bda9b4d270b39
Anders Henja <anders@baltrad.eu>
2022-12-16 08:36:22 +0100

    More doc-stuff

--- misc/build_github_doc.sh
+++ misc/build_github_doc.sh
@@ -6,7 +6,8 @@ PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
 
 # First setup environment
 sudo apt-get update
-sudo apt-get -y install git rsync python3-sphinx python3-pycryptodome
+sudo apt-get -y install git rsync python3-sphinx python3-pycryptodome python3-daemon
+sudo apt-get -y install python3-sqlalchemy python3-paramiko python3-pyinotify python3-scp
 
 git config --local user.email "baltrad@users.noreply.github.com"
 git config --local user.name "Baltrad GitHub Action"
