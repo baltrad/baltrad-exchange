c1689f4d28063097a201c806ccd30a820bcfa040
Anders <anders@baltrad.eu>
2023-12-05 09:05:56 +0100

    Fixed test-problems on buildserver

--- misc/runtest.sh
+++ misc/runtest.sh
@@ -2,14 +2,30 @@
 
 PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
 
+exit_with_message() {
+  echo "$*"
+  exit 127
+}
+
 create_and_init_venv() {
   echo "Creating virtual environment"
   envpath=$1
-  python3 -m venv --system-site-packages "$envpath"
+  if [ -d "$envpath" ]; then
+    \rm -fr "$envpath"
+  fi
+  python3 -m venv "$envpath"
   source $envpath/bin/activate
+  pip3 install --upgrade pip setuptools==57.5.0 
+  pip3 install "nose2 >= 0.12" --trusted-host pypi.python.org
+  pip3 install "mock >= 1.0" --trusted-host pypi.python.org
+  pip3 install "zmq" --trusted-host pypi.python.org
+  pip3 install "cryptography == 3.3.1" --trusted-host pypi.python.org
+  python3 -m pip install 'git+https://github.com/baltrad/baltrad-utils.git' || exit_with_message "Could not install baltrad-utils"
+  python3 -m pip install 'git+https://github.com/baltrad/baltrad-crypto.git' || exit_with_message "Could not install baltrad-crypto"
+  python3 -m pip install 'git+https://github.com/baltrad/baltrad-db.git/#egg=baltrad-bdbcommon&subdirectory=common' || exit_with_message "Could not install bdbcommon"
+  python3 -m pip install 'git+https://github.com/baltrad/baltrad-db.git/#egg=baltrad-bdbclient&subdirectory=client/python' || exit_with_message "Could not install bdbclient"
+
   python3 setup.py develop
-  $envpath/bin/pip3 install "nose2 >= 0.12" --trusted-host pypi.python.org
-  $envpath/bin/pip3 install "mock >= 1.0" --trusted-host pypi.python.org
 }
 
 runtest() {
